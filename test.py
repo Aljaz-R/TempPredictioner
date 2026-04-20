34b8049a4afbef682386a78fdc135ba3ff95f57d
AljazRant <aljaz.rant@gmail.com>
2026-04-20 14:00:32 +0200

    Test file update

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from data.clients.openweather_client import fetch_current_weather
-from data.loaders.cities_loader import load_active_cities
-from data.storage.raw_saver import save_raw_payload
+from clients.openweather_client import fetch_current_weather
+from loaders.cities_loader import load_active_cities
+from storage.raw_saver import save_raw_payload
 
 sample = {"test": 123}
 path = save_raw_payload("ljubljana", sample)
